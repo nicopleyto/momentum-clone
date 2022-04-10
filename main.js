91463f0c15832ce98936fa26ca6250bb460f1557
nicopleyto <nicopleyto@gmail.com>
2022-04-11 02:51:43 +0800

    fixed background image issue with wider screens + refactored

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-// background change every 1 hour
-backgroundSelection = ['https://images.unsplash.com/photo-1529928750697-1d9646312221?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80',
-'https://images.unsplash.com/photo-1548439935-5f5f0a4fb02e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80',
-'https://images.unsplash.com/photo-1548439935-9e1390d83250?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80',
-'https://images.unsplash.com/photo-1545768076-c58b243b8f3e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1548&q=80',
-'https://images.unsplash.com/photo-1561084195-ee7372303a19?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1471&q=80',
-'https://images.unsplash.com/photo-1563985336376-568060942b80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1476&q=80',
-'https://images.unsplash.com/photo-1515248101569-f6e91c070aa8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80']
-let random = Math.floor(Math.random() * backgroundSelection.length)
-document.querySelector('.background').style.background = `linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)), url('${backgroundSelection[random]}')`
+//random background every 1 hour
+backgroundSelection = [
+    'images/arctic-forest.jpeg',
+    'images/bridge.jpeg',
+    'images/cloudy-mountain.jpeg',
+    'images/desert.jpeg',
+    'images/green-lake.jpeg',
+    'images/rock-formation-beach.jpeg'
+]
 
-setInterval( () => { 
+function randomBackgroundImage () {
     let random = Math.floor(Math.random() * backgroundSelection.length)
-    document.querySelector('.background').style.background = `linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)), url('${backgroundSelection[random]}')` 
-    }, 3600000
-);
+    document.querySelector('.background').style.background = `linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)), url('${backgroundSelection[random]}') no-repeat`
+    document.querySelector('.background').style.backgroundSize = "cover"
+}
+
+randomBackgroundImage()
+setInterval(randomBackgroundImage, 3600000)
 
 // current time 
 function realtimeClock() {
@@ -38,7 +40,7 @@ if (localStorage.getItem('name')) {
     document.querySelector('#focusGreeting').style.visibility = 'visible'
     document.querySelector('#focusInput').style.visibility = 'visible'
 
-    paragraph.addEventListener('dblclick', () => {
+    paragraph.addEventListener('click', () => {
         document.querySelector('.nameContainer').removeChild(paragraph)
         document.querySelector('.nameContainer').style.display = 'none'
         document.querySelector('#greeting').style.display = 'block'
@@ -70,7 +72,7 @@ document.querySelector('#nameInput').addEventListener('keypress', (e) => {
         document.querySelector('#focusGreeting').style.visibility = 'visible'
         document.querySelector('#focusInput').style.visibility = 'visible'
 
-        paragraph.addEventListener('dblclick', () => {
+        paragraph.addEventListener('click', () => {
             document.querySelector('.nameContainer').removeChild(paragraph)
             document.querySelector('.nameContainer').style.display = 'none'
             document.querySelector('#greeting').style.display = 'block'
@@ -86,22 +88,23 @@ document.querySelector('#nameInput').addEventListener('keypress', (e) => {
 })
 
 //quotes
-inspirationalQuotes = [`"Concentrate all your thoughts upon the work in hand. The sun's rays do not burn until brought to a focus." -Alexander Graham Bell`,
-`“Either you run the day or the day runs you.” -Jim Rohn`,
-`“I’m a greater believer in luck, and I find the harder I work the more I have of it.” -Thomas Jefferson`,
-`“When we strive to become better than we are, everything around us becomes better too.” -Paulo Coelho`
+inspirationalQuotes = [
+    `"Concentrate all your thoughts upon the work in hand. The sun's rays do not burn until brought to a focus." -Alexander Graham Bell`,
+    `“Either you run the day or the day runs you.” -Jim Rohn`,
+    `“I’m a greater believer in luck, and I find the harder I work the more I have of it.” -Thomas Jefferson`,
+    `“When we strive to become better than we are, everything around us becomes better too.” -Paulo Coelho`
 ]
 
-let randomTwo = Math.floor(Math.random() * inspirationalQuotes.length)
-document.querySelector('#quote').innerHTML = inspirationalQuotes[randomTwo]
+function randomQuote(){
+    let random = Math.floor(Math.random() * inspirationalQuotes.length)
+    document.querySelector('#quote').innerHTML = inspirationalQuotes[random]
+}
 
-setInterval( () => { 
-    let randomTwo = Math.floor(Math.random() * inspirationalQuotes.length)
-    document.querySelector('#quote').innerHTML = inspirationalQuotes[randomTwo]
-    }, 3600000
-)
+randomQuote()
+setInterval(randomQuote, 3600000)
 
-document.querySelector('#quote').addEventListener('dblclick', () => {
+
+document.querySelector('#quote').addEventListener('click', () => {
     document.querySelector('#quote').style.display = 'none'
     document.querySelector('#addQuote').style.display = 'flex'
 })
@@ -117,9 +120,8 @@ document.querySelector('#addQuote').addEventListener('keypress', (e)=> {
     }
 })
 
-document.querySelector('#addQuote').addEventListener('dblclick', () => {
-    let randomTwo = Math.floor(Math.random() * inspirationalQuotes.length)
-    document.querySelector('#quote').innerHTML = inspirationalQuotes[randomTwo]
+document.querySelector('#addQuote').addEventListener('click', () => {
+    randomQuote()
     document.querySelector('#addQuote').style.display = 'none'
     document.querySelector('#quote').style.display = 'flex'
 })
